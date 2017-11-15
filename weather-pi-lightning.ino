f315df281be62b734e343bcf0b53f7f4e4dfd3f7
Michael <michael.rosenauer@googlemail.com>
2017-11-15 22:30:21 +0100

    added icons.h displaying lightning distance

--- weather-pi-lightning.ino
+++ weather-pi-lightning.ino
@@ -1,33 +1,153 @@
+#include <Ticker.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <WiFiClient.h>
 #include <Wire.h>
 #include <ArduinoJson.h>
-#include <AS3935.h>
+#include <Adafruit_GFX.h>
+#include <Fonts/FreeSans24pt7b.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_HTU21DF.h>
+#include "AS3935.h"
+#include "icons.h"
+#include "credentials.h"
 
-#define IRQ_PIN 2
+#define IRQ_PIN 4
+#define OLED_RESET 0
 
-volatile bool detected = false;
+enum Event {
+  NOTHING,
+  NOISE,
+  DISTURBER,
+  LIGHTNING
+};
 
-const char* ssid         = "xxxxxxxx";
-const char* password     = "xxxxxxxx";
-const char* mqttServer   = "xxxxxxxx";
-const char* mqttUser     = "xxxxxxxx";
-const char* mqttPassword = "xxxxxxxx";
+Adafruit_SSD1306 display(OLED_RESET);
+Adafruit_HTU21DF htu = Adafruit_HTU21DF();
 
-const String place = "LIGHTNING";
-const String sensordata = "sensordata";
-const String typeDistance = "DISTANCE";
+volatile bool readyForHTUUpdate = true;
+
+Ticker htuUpdateTicker;
+Ticker currentDisplayTicker;
+Ticker irqDisplayTicker;
 
 
-char json[256];
+const char* place         = "LIGHTNING";
+const char* sensordata    = "sensordata";
+const char* typeLightning = "LIGHTNING";
+
+const char* topic = "sensordata/lightning/lightning";
+
+unsigned long          previousMillis  = 0;     
+
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-DynamicJsonBuffer jsonBuffer;
-JsonObject& obj = jsonBuffer.createObject();
+char json[256];
+
+// used to store the data that should be displayed
+struct displayData_t {
+    float temperature;
+    float humidity;
+    bool  detected;
+    int   distance;
+    Event event;
+    bool  wifiConnected;
+    bool  mqttConnected;
+};    
+
+volatile displayData_t displayData = { 0.0, 0.0, false, 1, NOTHING, false, false};
+
+const int    displaysCount      = 2;
+volatile int currentDisplayIndx = 0;
+
+const int icon_wifi_pos_x = 0;  
+const int icon_wifi_pos_y = 1; 
+const int icon_data_pos_x = icon_wifi_pos_x;  
+const int icon_data_pos_y = icon_wifi_pos_y + 22;  
+const int icon_disturber_pos_x = icon_wifi_pos_x;  
+const int icon_disturber_pos_y = icon_data_pos_y + 22; 
+const int icon_noise_pos_x = icon_wifi_pos_x;  
+const int icon_noise_pos_y = icon_data_pos_y + 22;
+const int data_pos_x = 30;
+
+void displayTemperature() {
+    display.setFont(&FreeSans24pt7b);
+    display.setCursor(data_pos_x,(display.height() + 24) / 2);
+    display.setTextSize(1);
+    display.print(displayData.temperature,1);
+    display.drawBitmap(icon_data_pos_x,icon_data_pos_y, temperature_icon_bmp, temperature_icon_width, temperature_icon_height, WHITE); 
+    display.setFont();
+}
+
+void displayHumidity() {
+    display.setFont(&FreeSans24pt7b);
+    display.setCursor(data_pos_x,(display.height() + 24) / 2);
+    display.setTextSize(1);
+    display.print(displayData.humidity,0);
+    display.drawBitmap(icon_data_pos_x,icon_data_pos_y, humidity_icon_bmp, humidity_icon_width, humidity_icon_height, WHITE); 
+    display.setFont();
+}
+
+void displayLightning() {
+    display.setFont(&FreeSans24pt7b);
+    display.setCursor(data_pos_x,(display.height() + 24) / 2);
+    display.setTextSize(1);
+    display.print(displayData.distance);
+    display.drawBitmap(icon_data_pos_x,icon_data_pos_y, lightning_icon_bmp, lightning_icon_width, lightning_icon_height, WHITE); 
+    display.setFont();    
+
+/*
+      if (displayData.distance == -1)
+    display.println("Lightning out of range");
+  else if (displayData.distance == 1)
+    display.println("Distance not in table");
+  else if (displayData.distance == 0)
+    display.println("Lightning overhead");
+  else {
+    display.print("Lightning ~");
+    display.print(displayData.distance);
+    display.println("km away\n");  
+  }
+  */
+}
     
+
+
+void (*displays[displaysCount])(void)= {displayTemperature, displayHumidity};
+
+void updateDisplay() {
+    
+    display.clearDisplay();
+    
+    if (displayData.wifiConnected) {        
+        display.drawBitmap(icon_wifi_pos_x,icon_wifi_pos_y, wifi_icon_bmp, wifi_icon_width, wifi_icon_height, WHITE);    
+    } else {
+        display.drawBitmap(icon_wifi_pos_x,icon_wifi_pos_y, no_wifi_icon_bmp, no_wifi_icon_width, no_wifi_icon_height, WHITE);    
+    }    
+
+    if (displayData.event == NOISE) {        
+        display.drawBitmap(icon_noise_pos_x,icon_noise_pos_y, noise_icon_bmp, noise_icon_width, noise_icon_height, WHITE);    
+    } else if (displayData.event == DISTURBER) {        
+        display.drawBitmap(icon_disturber_pos_x,icon_disturber_pos_y, disturber_icon_bmp, disturber_icon_width, disturber_icon_height, WHITE);    
+    } 
+
+    if (displayData.event == LIGHTNING) {
+        displayLightning();
+    } else {
+        (*displays[currentDisplayIndx])();    
+    }
+    
+    
+    display.display();   
+}
+
+void readHTUData() {
+    displayData.temperature = htu.readTemperature();
+    displayData.humidity    = htu.readHumidity();
+}
+
 /**************************************************************************/
 /*
     Try to connect to the WIFI, after 10 tries, we do a deep sleep
@@ -58,114 +178,149 @@ boolean mqttConnect(void) {
   client.setServer(mqttServer, 1883);
 
   int  tries = 10;
+  displayData.mqttConnected = false;
   while (!client.connected()) {
     tries--;
     if (tries == 0) {
       return false;
     }
-    if (!client.connect(place.c_str(), mqttUser, mqttPassword)) {
+    if (!client.connect(place, mqttUser, mqttPassword)) {
       delay(5000);
     }
   }
+  displayData.mqttConnected = true;
   return true;
 }
 
-void sendAS3935Distance(JsonObject& jsonObject, int distance) {
-  
-  String topic = sensordata + "/" + place + "/" + typeDistance;
-  topic.toLowerCase();
-  
-  jsonObject.set("sensor", "AS3935");
-  jsonObject.set("type", typeDistance);
-  jsonObject.set("distance", distance);
+void sendAS3935Distance(int distance) {
 
-  jsonObject.printTo(json, sizeof(json));
-  client.publish(topic.c_str(), json);
-  jsonObject.remove("distance");
-  jsonObject.remove("type");
-  jsonObject.remove("sensor");
-  delay(250);
+    StaticJsonBuffer<256> jsonBuffer;
+    JsonObject&           root = jsonBuffer.createObject();
+
+    root.set("place", place);
+    root.set("sensor", "AS3935");
+    root.set("type", typeLightning);
+    root.set("distance", distance);
+
+    root.printTo(json, sizeof(json));
+    client.publish(topic, json);
+    delay(250);
 }
+
 void setup() {
-  Serial.begin(115200);
-  while (!Serial) {}
-  Serial.println("Welcome to the MOD-1016 (AS3935) Lightning Sensor test sketch!");
-  Serial.println("Embedded Adventures (www.embeddedadventures.com)\n");
 
-  //I2C
-  Wire.begin();
-  mod1016.init(IRQ_PIN);
+    //I2C
+    Wire.begin();
   
- 
-  //Tune Caps, Set AFE, Set Noise Floor
-  //autoTuneCaps(IRQ_PIN);
-  
-  mod1016.setTuneCaps(5);
-  mod1016.setOutdoors();
-  mod1016.setNoiseFloor(5);
-  
-  
-  Serial.println("TUNE\tIN/OUT\tNOISEFLOOR");
-  Serial.print(mod1016.getTuneCaps(), HEX);
-  Serial.print("\t");
-  Serial.print(mod1016.getAFE(), BIN);
-  Serial.print("\t");
-  Serial.println(mod1016.getNoiseFloor(), HEX);
-  Serial.print("\n");
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
+    display.clearDisplay();
+    display.display();
 
-  pinMode(IRQ_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(IRQ_PIN), alert, RISING);
-  mod1016.getIRQ();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+    display.println("AS3935");
 
-  Serial.println("connect to WIFI...");
-  // connect to Wifi
-  wifiConnect();
+    display.display();
+  
+    htu.begin();
 
-  obj.set("place", place);
+    // initialize the AS3935
+    mod1016.init(IRQ_PIN);    
+    mod1016.setTuneCaps(5);  // 
+    mod1016.setIndoors();
+    //mod1016.setOutdoors();
+    mod1016.setNoiseFloor(3);
+    //mod1016.setMinimumLightnings(MIN_NUM_LGHT_5);
+    
+    display.print("TUNE        :");
+    display.println(mod1016.getTuneCaps(), HEX);
+    display.print("IN/OUT      :");
+    display.println(mod1016.getAFE(), BIN);
+    display.print("NOISEFLOOR  :");
+    display.println(mod1016.getNoiseFloor(), HEX);
+    display.print("MIN_NUM_LGHT:");
+    display.println(mod1016.getMinimumLightnings(),HEX);
+
+    display.display();
+    pinMode(IRQ_PIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(IRQ_PIN), alert, RISING);
+    mod1016.getIRQ();
+
+    display.print("connect to WIFI...");
+    display.display();
+    // connect to Wifi
+    displayData.wifiConnected = wifiConnect();
+    updateDisplay();
+    display.setTextSize(1);
+    
+
+    htuUpdateTicker.attach(30, setReadyForHTUUpdate);
+    currentDisplayTicker.attach(2,nextDisplay);
+}
+
+/**
+ * Functions used by the different ticker
+ */
+void setReadyForHTUUpdate() {
+    readyForHTUUpdate = true;  
+}
+
+void nextDisplay() {
+    currentDisplayIndx = (currentDisplayIndx + 1) % displaysCount;
+}
+
+void resetIRQDisplayData() {
+    displayData.event = NOTHING;
 }
 
 void loop() {
-  if (detected) {
-    translateIRQ(mod1016.getIRQ());
-    detected = false;
-  }
-  client.loop();
+    unsigned long currentMillis = millis();
+    if (displayData.detected) {        
+        translateIRQ(mod1016.getIRQ());
+        displayData.detected = false;
+        previousMillis = currentMillis;            
+    }
+    client.loop();
+
+  
+    if (currentMillis - previousMillis >= 2000) {
+        updateDisplay();
+        previousMillis = currentMillis;    
+    }
+
+    if (readyForHTUUpdate) {
+        readHTUData();
+        readyForHTUUpdate = false;    
+    }
+
 }
 
+// gets called from the AS3935 interrupt 
 void alert() {
-  detected = true;
+  displayData.detected = true;
 }
 
-void translateIRQ(uns8 irq) {
-  switch(irq) {
-      case 1:
-        Serial.println("NOISE DETECTED");
+void translateIRQ(byte irq) {
+    displayData.event = NOTHING;
+    irqDisplayTicker.detach();
+    switch(irq) {
+      case 1:        
+        displayData.event = NOISE;
         break;
       case 4:
-        Serial.println("DISTURBER DETECTED");
+        displayData.event = DISTURBER;
         break;
       case 8: 
-        Serial.println("LIGHTNING DETECTED");
-        printDistance();
+        displayData.event = LIGHTNING;
+        displayData.distance = mod1016.calculateDistance();
+        if (mqttConnect()) {
+            sendAS3935Distance(displayData.distance);
+        }        
         break;
     }
+    irqDisplayTicker.once(10, resetIRQDisplayData);
+    updateDisplay();
 }
 
-void printDistance() {
-  int distance = mod1016.calculateDistance();
-  if (mqttConnect()) {
-    sendAS3935Distance(obj,distance);
-  }
-  if (distance == -1)
-    Serial.println("Lightning out of range");
-  else if (distance == 1)
-    Serial.println("Distance not in table");
-  else if (distance == 0)
-    Serial.println("Lightning overhead");
-  else {
-    Serial.print("Lightning ~");
-    Serial.print(distance);
-    Serial.println("km away\n");  
-  }
-}
 
